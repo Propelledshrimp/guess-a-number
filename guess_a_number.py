215ecea95864c07f21b0aea1dd2d293686650fbf
Tatem Pearson <jpears1974@greenvilleschools.us>
2017-09-14 09:09:52 -0400

    Made the game more advanced.

--- guess_a_number.py
+++ guess_a_number.py
@@ -3,13 +3,17 @@ import random
 #  config
 low = 1
 high = 100
+limit= 10 
 
+#start game
 rand = random.randint(low, high)
 print("I'm thinking of a number from " + str(low) + " to " + str(high) + " .");
 
 guess = -1
+tries = 0
 
-while guess != rand:
+#gameplay
+while guess != rand and tries < limit: 
     guess = input("Take a guess: ")
     guess = int(guess)
     
@@ -17,7 +21,12 @@ while guess != rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
-    else:
-        print("You got it!")
+    
+    tries += 1 
+#tell player outcome
+    
+if guess == rand:
+    print("You win!")
+else:
+    print("You lose. The number I was thinking of is " + str(rand) + ".") 
 
-print("Game over")
