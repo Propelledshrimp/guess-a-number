7afb48a491f49217d1386214d8cdc99767e511c9
Tatem Pearson <jpears1974@greenvilleschools.us>
2017-09-20 09:25:13 -0400

    reformatted the code and added a title and credits screen

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,58 +1,90 @@
+
 import random
 
-#  config
+# config
 low = 1
 high = 100
-limit= 10
+limit = 10
 
+# helper functions
+def show_start_screen():
+    print("   _____                               _   _                 _               _ ") 
+    print("  / ____|                             | \ | |               | |             | |") 
+    print(" | |  __ _   _  ___  ___ ___    __ _  |  \| |_   _ _ __ ___ | |__   ___ _ __| |") 
+    print(" | | |_ | | | |/ _ \/ __/ __|  / _` | | . ` | | | | '_ ` _ \| '_ \ / _ \ '__| |")  
+    print(" | |__| | |_| |  __/\__ \__ \ | (_| | | |\  | |_| | | | | | | |_) |  __/ |  |_|")  
+    print("  \_____|\__,_|\___||___/___/  \__,_| |_| \_|\__,_|_| |_| |_|_.__/ \___|_|  (_)")  
+                                                                                   
+
+def show_credits():
+    print("This awesome game was created by ")
+    print("  _______    _                   _____ ")                                
+    print(" |__   __|  | |                 |  __ \ ")                               
+    print("    | | __ _| |_ ___ _ __ ___   | |__) |__  __ _ _ __ ___  ___  _ __ ") 
+    print("    | |/ _` | __/ _ \ '_ ` _ \  |  ___/ _ \/ _` | '__/ __|/ _ \| '_ \\")   
+    print("    | | (_| | ||  __/ | | | | | | |  |  __/ (_| | |  \__ \ (_) | | | |")
+    print("    |_|\__,_|\__\___|_| |_| |_| |_|   \___|\__,_|_|  |___/\___/|_| |_|") 
+    
 def get_guess():
     while True:
-        g = input("Take a guess: ")
+        guess = input("Guess a number: ")
 
-        if g.isnumeric():
-            g = int(g)
-            return g
+        if guess.isnumeric():
+            guess = int(guess)
+            return guess
         else:
             print("You must enter a number.")
-def play_again():
-    while True:
-        decision = input("Would you like to play again? (y/n) ") 
 
-        if decision == "y" or decision == "yes":
-                         return True
-        elif decision == "n" or decision == "no":
-                         return False
+def pick_number():
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) +".")
 
-        print("I don't understand. Please answer 'y' or 'n' .")
+    return random.randint(low, high)
 
-again = True
+def check_guess(guess, rand):
+    if guess < rand:
+        print("You guessed too low.")
+    elif guess > rand:
+        print("You guessed too high.")
 
-while again: 
-    #start game
-    rand = random.randint(low, high)
-    print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
-
-    guess = -1
-    tries = 0
-
-    #gameplay
-    while guess != rand and tries < limit: 
-        guess = get_guess()  
-
-        if guess < rand:
-            print("You guessed too low.")
-        elif guess > rand:
-            print("You guessed too high.")
-        
-        tries += 1
-        
-    #tell player outcome
+def show_result(guess, rand):
     if guess == rand:
         print("You win!")
     else:
-        print("You lose. The number I was thinking of is " + str(rand) + ".")
+        print("Too bad! The number was " + str(rand) + ".")
 
-    again = play_again()
+def play_again():
+    while True:
+        decision = input("Would you like to play again? (y/n) ")
 
-print("Thank you for playing!") 
+        if decision == 'y' or decision == 'yes':
+            return True
+        elif decision == 'n' or decision == 'no':
+            return False
+        else:
+            print("I don't understand. Please enter 'y' or 'n'.")
 
+def play():
+    guess = -1
+    tries = 0
+
+    rand = pick_number()
+    
+    while guess != rand and tries < limit:
+        guess = get_guess()
+        check_guess(guess, rand)
+
+        tries += 1
+
+    show_result(guess, rand)
+
+
+# Game starts running here
+show_start_screen()
+
+playing = True
+
+while playing:
+    play()
+    playing = play_again()
+
+show_credits()
